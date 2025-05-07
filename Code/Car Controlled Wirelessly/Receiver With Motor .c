c02918abf8b75368e923ed8962db1e94d10986bb
Ruben Jovanka Bryan Sahetapy <bryan.sahetapy@gmail.com>
2025-05-08 01:36:01 +0200

    New codew for prototype V2

--- Code/Car Controlled Wirelessly/Receiver With Motor .c
+++ Code/Car Controlled Wirelessly/Receiver With Motor .c
@@ -2,62 +2,107 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8);
+RF24 radio(7, 8); // CE, CSN pins
 const byte address[6] = "00001";
 
-// CONFIRMED CORRECT PINS AFTER TESTING
-const int leftFront = 2;
-const int leftBack = 3;
-const int rightFront = 5;
-const int rightBack = 6;
+// Motor pins (using your exact setup)
+const int motor1 = 2;
+const int motor2 = 3;
+const int motor3 = 5;
+const int motor4 = 6;
 
 struct GyroData {
-  float angleX;
-  float angleY;
+  float angleX; // Left/Right tilt
+  float angleY; // Forward/Backward tilt
 };
 
 void setup() {
   Serial.begin(9600);
-  pinMode(leftFront, OUTPUT);
-  pinMode(leftBack, OUTPUT);
-  pinMode(rightFront, OUTPUT);
-  pinMode(rightBack, OUTPUT);
   
+  // Initialize motor pins
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
+  pinMode(motor3, OUTPUT);
+  pinMode(motor4, OUTPUT);
+  
+  // Initialize radio
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 
-  Serial.println("Receiver Ready!");
+  Serial.println("System Ready");
+}
+
+void stopMotors() {
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, LOW);
+  digitalWrite(motor3, LOW);
+  digitalWrite(motor4, LOW);
+}
+
+void moveForward() {
+  // Your exact forward pattern
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, LOW);
+  Serial.println("Moving Forward");
+}
+
+void moveBackward() {
+  // Inverse of your forward pattern
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, LOW);
+  digitalWrite(motor3, LOW);
+  digitalWrite(motor4, HIGH);
+  Serial.println("Moving Backward");
+}
+
+void turnRight() {
+  // Left side forward, right side backward (sharp turn)
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3, LOW);
+  digitalWrite(motor4, HIGH);
+  Serial.println("Turning Right");
+}
+
+void turnLeft() {
+  // Right side forward, left side backward (sharp turn)
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, LOW);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, LOW);
+  Serial.println("Turning Left");
 }
 
 void loop() {
   if (radio.available()) {
     GyroData data;
     radio.read(&data, sizeof(data));
-    
-    // Reset motors
-    digitalWrite(leftFront, LOW);
-    digitalWrite(leftBack, LOW);
-    digitalWrite(rightFront, LOW);
-    digitalWrite(rightBack, LOW);
-    
-    // Movement logic
-    if (data.angleY > 15) { // Forward
-      digitalWrite(leftFront, HIGH);
-      digitalWrite(rightFront, HIGH);
+
+    // Invert angleY to fix left/right confusion
+    data.angleY = -data.angleY;
+
+    stopMotors();
+
+    // angleX = forward/backward tilt
+    // angleY = left/right tilt
+    if (data.angleX > 15) {
+      moveForward();
     } 
-    else if (data.angleY < -15) { // Backward
-      digitalWrite(leftBack, HIGH);
-      digitalWrite(rightBack, HIGH);
+    else if (data.angleX < -15) {
+      moveBackward();
     } 
-    else if (data.angleX > 15) { // Right
-      digitalWrite(leftFront, HIGH);
-      digitalWrite(rightBack, HIGH);
+    else if (data.angleY > 15) {
+      turnRight();
     } 
-    else if (data.angleX < -15) { // Left
-      digitalWrite(leftBack, HIGH);
-      digitalWrite(rightFront, HIGH);
+    else if (data.angleY < -15) {
+      turnLeft();
+    }
+    else {
+      Serial.println("Stopped");
     }
   }
 }
